8ee405aa49562af79a0ad93a09e6c1257b17ad24
Michael Pratt <commits@michael-pratt.com>
2020-01-25 23:35:27 -0500

    Updated archlinux packages list

--- packages/archlinux/archlinux-pkgs.sh
+++ packages/archlinux/archlinux-pkgs.sh
@@ -9,30 +9,47 @@ CURRENTLOCATION="${ROOTLOCATION}/packages/archlinux"
 NOTINSTALLED=()
 CMD="pacman"
 
-if [[ "$(whoami)" != "root" ]]; then
-    echo "On Archlinux, you need to be root in order to install packages"
-    exit 0
+if [[ "$(whoami)" == "root" ]]; then
+    # Updating the system
+    ${CMD} -Syu
+
+    # Install Git
+    ${CMD} -S --noconfirm git
+
+    # Install Linux Zen Kernels
+    ${CMD} -S linux-zen
+    ${CMD} -S linux-zen-headers
+
+    # Install Linux LTS Kernels
+    ${CMD} -S linux-lts
+    ${CMD} -S linux-lts-headers
+
+    # Install Linux Nvidia Drivers
+    ${CMD} -S --no-confirm nvidia-dkms
+    ${CMD} -S --no-confirm nvidia-settings
+    ${CMD} -S --no-confirm nvidia-utils
+
+    LIST=$(grep -v '^$\|^\s*\#' ${CURRENTLOCATION}/packages.list)
+    for pkg in ${LIST}; do
+        echo "Installing: ${pkg}"
+        ${CMD} -S --noconfirm --needed ${pkg}
+        echo ""
+        echo ""
+    done
 fi
 
-# Updating the system
-${CMD} -Syu
 
-# Install Linux LTS Kernels
-${CMD} -S --needed linux-lts
-${CMD} -S --needed linux-lts-headers
-
-LIST=$(grep -v '^$\|^\s*\#' ${CURRENTLOCATION}/packages.list)
-for pkg in ${LIST}; do
-    echo "Installing: ${pkg}"
-    ${CMD} -S --noconfirm --needed ${pkg}
-    echo ""
-    echo ""
-done
+# Install Yay
+if ! [ -e "/usr/bin/yay" ]; then
+    git clone https://aur.archlinux.org/yay.git ~/yay-tmp
+    cd ~/yay-tmp
+    makepkg -si
+fi
 
 LIST=$(grep -v '^$\|^\s*\#' ${CURRENTLOCATION}/aur-packages.list)
 for pkg in ${LIST}; do
     echo "Installing: ${pkg}"
-    #${CMD} -S --noconfirm ${pkg}
+    yay -S  --noconfirm ${pkg}
     echo ""
     echo ""
 done
